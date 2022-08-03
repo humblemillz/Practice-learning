a10867c99418714d73826fec856578387811e36c
humblemillz <humblemillions@gmail.com>
2022-08-03 02:21:51 -0700

    learningtoturial

---
+++ learning/learning.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	/*
+		do
+		{
+		// statements
+		} while (<condition>);
+	*/
+	int i = 5;
+	int j = 5;
+
+	do
+{
+	printf("%d", ++i);
+	} while (i < 5);
+
+}
+
+return (0);
